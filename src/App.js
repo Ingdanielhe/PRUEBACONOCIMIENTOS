dfed2c9944102f5b165477905af8dfda21e9e023
ingdanielhe <ingdanielhe@gmail.com>
2022-06-17 18:55:12 -0500

    agregando todos los archivos al commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Chuck from './Assets/IMG_20220615_214302.png';
+import Categories from './components/Categories/Categories';
+import Joke from './components/Joke/Joke';
+import chuckIcon from "./Assets/Chuck-Norris-No-Background.png"
+
+import './App.scss';
+
+function App() {
+  return (
+    <section className='App'>
+      <h1 className='principal-title'>Chuck Norris Jokes <img src={chuckIcon} alt="chuck-norris-icon" width="70px" height="70px" /></h1>
+      <div className='main'>
+        <div className='container-data'>
+          <img src={Chuck} alt='chuc-norris-img' width="400px" height="300px" />
+          <Categories />
+        </div>
+        <Switch>
+          {['/', '/random', '/random/'].map((path) => (
+            <Route
+              key={path}
+              exact
+              path={path}
+              render={(props) => <Joke key={props.location.key} />}
+            />
+          ))}
+        </Switch>
+      </div>
+    </section>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter basename='/react-chucknorris-api'>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
